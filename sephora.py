3dba00bdfb3bb12a077605607cba236188684a67
Emma Peterson <emmacooperpeterson@gmail.com>
2019-05-20 22:58:06 -0500

    this kinda works

--- sephora.py
+++ sephora.py
@@ -9,69 +9,23 @@ PRICE_CLASS = "css-14hdny6" #div
 PRODUCT_TYPE_CLASS = "css-or7ouu" #a
 PRODUCT_LINK_CLASS = "css-ix8km1" #a
 
-class Product:
-    def __init__(self, link_suffix):
-        self.base_url = "https://www.sephora.com"
-        self.link_suffix = link_suffix
-        self.name = None
-        self.brand = None
-        self.price = None
-        self.description = None
-        self.usage = None
-        self.ingredients = None
-
-    def get_product_details(self):
-        page = requests.get(self.base_url + self.link_suffix)
-        soup = bs(page.content, 'html.parser')
-
-        product_details = soup.find_all("div", class_ = PRODUCT_CLASS)
-
-        if len(product_details) > 0:
-            self.description = product_details[0].get_text()
-
-        if len(product_details) > 1:
-            self.usage = product_details[1].get_text()
-
-        if len(product_details) > 2:
-            self.ingredients = product_details[2].get_text()
-
-        brand = soup.find("span", class_ = BRAND_CLASS).get_text()
-        self.brand = brand
-
-        name = soup.find("span", class_ = NAME_CLASS).get_text()
-        self.name = name
-
-        price = soup.find("div", class_ = PRICE_CLASS).get_text()
-        self.price = price
-
-    def clean_ingredients(self):
-        regex = r"-.*\:.*\.\s(.*)"
-        matches = re.findall(regex, self.ingredients)
-        if matches != []:
-            matches_list = matches[0].split(",")
-            clean_matches = [m.strip().lower().replace(".", "") for m in matches_list]
-            self.ingredients = clean_matches
-
 
 
 class Sephora:
     def __init__(self):
         self.product_type_link_class = PRODUCT_TYPE_CLASS
         self.base_url = "https://www.sephora.com/"
-        self.names = {"skincare": "skincare", "makeup": "makeup-cosmetics"}
-        self.categories = []
+        self.category_links = []
+        self.product_links = []
+        self.product_info = []
 
-    def get_categories(self, category_type):
-        url = self.base_url + "shop/" + self.names[category_type]
+    def get_category_links(self, category_name):
+        # get links for each category page
+        url = self.base_url + "shop/" + category_name
         page = requests.get(url)
         soup = bs(page.content, 'html.parser')
         categories = soup.find_all("a", class_ = self.product_type_link_class)
-        links = [c["href"] for c in categories]
 
-        for link in links:
-            self.categories.append(link)
-
-    def remove_categories(self):
         exclude = [
             "/shop/skin-care-sets-travel-value",
             "/shop/mini-skincare",
@@ -84,7 +38,37 @@ class Sephora:
             "/shop/nails-makeup"
             ]
 
-        self.categories = [p for p in self.categories if p not in exclude]
+        links = [c["href"] for c in categories if c not in exclude]
+        self.category_links += links
+
+    def get_product_links(self):
+        # get links to every product listed on every category page
+        products = []
+        for link in self.category_links[15:16]: # subsetting for testing purposes
+            category = Category(link)
+            category.get_category()
+            products.append(category.products)
+
+        self.product_links = [p for lst in products for p in lst]
+
+
+    def get_all_product_info(self):
+        # get product info for every product listed on every category page
+        for link in self.product_links:
+            product = Product(link)
+            product.get_product_details()
+            product.clean_ingredients()
+
+            self.product_info.append({
+                product.name: {
+                    "link": product.base_url + product.link_suffix,
+                    "brand": product.brand,
+                    "price": product.price,
+                    "raw ingredients": product.raw_ingredients,
+                    "ingredients": product.ingredients
+                }
+            })
+
 
 
 class Category:
@@ -95,7 +79,8 @@ class Category:
         self.products = []
         self.test = None
 
-    def get_products(self):
+    def get_category(self):
+        # get links to every product in a given category
         url = self.base_url + self.category_name
         page = requests.get(url)
         soup = bs(page.content, "html.parser")
@@ -107,33 +92,82 @@ class Category:
 
 
 
-if __name__ == '__main__': # put all of this into the Sephora class
+class Product:
+    def __init__(self, link_suffix):
+        self.base_url = "https://www.sephora.com"
+        self.link_suffix = link_suffix
+        self.name = None
+        self.brand = None
+        self.price = None
+        self.description = None
+        self.usage = None
+        self.ingredients = None
+        self.raw_ingredients = None
+        self.first = None
+        self.rest = None
+
+    def get_product_details(self):
+        # get information about a given product
+        page = requests.get(self.base_url + self.link_suffix)
+        soup = bs(page.content, 'html.parser')
+
+        product_details = soup.find_all("div", class_ = PRODUCT_CLASS)
+
+        if len(product_details) > 0:
+            self.description = product_details[0].get_text()
+
+        if len(product_details) > 1:
+            self.usage = product_details[1].get_text()
+
+        if len(product_details) > 2:
+            self.raw_ingredients = product_details[2].get_text()
+
+        brand = soup.find("span", class_ = BRAND_CLASS).get_text()
+        self.brand = brand
+
+        name = soup.find("span", class_ = NAME_CLASS).get_text()
+        self.name = name
+
+        price = soup.find("div", class_ = PRICE_CLASS).get_text()
+        self.price = price
+
+    def clean_ingredients(self):
+        # remove parentheticals
+        regex = r"\({1}.{1,20}\){1}\s"
+        self.raw_ingredients = re.sub(regex, "", self.raw_ingredients)
+
+        # remove hyphens
+        regex = r"\-"
+        self.raw_ingredients = re.sub(regex, " ", self.raw_ingredients)
+
+        # remove weird line breaks / characters
+        for pattern in [u"\xa0", u"\u2028", u"\r", u"\t", u"\n"]:
+            self.raw_ingredients = self.raw_ingredients.replace(pattern, u"")
+
+        first_ingredient_regex = r"\.([A-Z]+[\w\s]*)\,|[\.\s?]\s([A-Z]+[\w\s]*)\,"
+        remaining_ingredients_regex = r"[\,]\s([A-Z]+[\w\s]*)"
+
+        ingredients = []
+        first_ingredient = re.search(first_ingredient_regex, self.raw_ingredients)
+
+        if not first_ingredient:
+            first_ingredient = re.search(r"([A-Z][a-z]*)\,", self.raw_ingredients)
+
+        if first_ingredient:
+            ingredients.append(first_ingredient.group().strip("., \r"))
+        else:
+            print(self.raw_ingredients)
+
+        ingredients += re.findall(remaining_ingredients_regex, self.raw_ingredients)
+
+        self.ingredients = [ingr.strip() for ingr in ingredients]
+
+
+
+
+if __name__ == '__main__':
     sephora = Sephora()
-
-    for category in ["makeup", "skincare"]:
-        sephora.get_categories(category)
-
-    sephora.remove_categories()
-
-    products = []
-    for link in sephora.categories[10:11]: # subsetting for testing purposes
-        category = Category(link)
-        category.get_products()
-        products.append(category.products)
-
-    products = [p for lst in products for p in lst]
-
-    all_products = []
-    for link in products:
-        product = Product(link)
-        product.get_product_details()
-        product.clean_ingredients()
-        all_products.append({product.name: {
-                                "link": product.base_url + product.link_suffix,
-                                "brand": product.brand,
-                                "price": product.price,
-                                #"description": product.description,
-                                #"usage": product.description,
-                                "ingredients": product.ingredients
-                                }
-                            })
+    sephora.get_category_links("makeup-cosmetics")
+    sephora.get_category_links("skincare")
+    sephora.get_product_links()
+    sephora.get_all_product_info()
